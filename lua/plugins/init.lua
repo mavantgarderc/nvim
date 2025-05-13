f0936073981c98aa1f9cb88c2f1e60105888d29e
Mava <manihabibinava@gmail.com>
2025-05-14 01:55:05 +0330

    reverted manually

---
+++ lua/plugins/init.lua
@@ -0,0 +1,28 @@
+return{
+    -- themes
+    "catppuccin/nvim",
+    "folke/tokyonight.nvim",
+    "ellisonleao/gruvbox.nvim", 
+    "rose-pine/neovim",
+    "navarasu/onedark.nvim",
+    "EdenEast/nightfox.nvim",
+
+    -- nvim plugins
+    "nvim-lualine/lualine.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-treesitter/nvim-treesitter",
+
+    -- lsp
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    
+    -- none-ls
+    "nvimtools/none-ls.nvim",
+
+    -- Dashboard
+    "goolord/alpha-nvim",
+}
