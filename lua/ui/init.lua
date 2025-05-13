0a4a2d67b0f9b2ad40d3941a84e5bb2b332e5ef1
Mava <manihabibinava@gmail.com>
2025-05-14 01:25:03 +0330

    ...

---
+++ lua/ui/init.lua
@@ -0,0 +1,13 @@
+return{
+    -- ui
+    "nvim-lualine/lualine.nvim",
+    -- Dashboard
+    "goolord/alpha-nvim",
+    -- themes
+    "catppuccin/nvim",
+    "folke/tokyonight.nvim",
+    "ellisonleao/gruvbox.nvim", 
+    "rose-pine/neovim",
+    "navarasu/onedark.nvim",
+    "EdenEast/nightfox.nvim",
+}
