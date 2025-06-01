949ebda9dcf321ea424cd08984bc7cda6d3ced71
mavantgarderc <manihabibinava@gmail.com>
2025-06-01 19:48:17 +0330

    debuged

--- init.lua
+++ init.lua
@@ -2,14 +2,9 @@
 require("core.bootstrap")
 require("core.options")
 require("core.keymaps")
-require("core.keymapviewer")
 require("core.themepicker")
 
 -- Plugins Directory
 require("lazy").setup("plugins")
 
--- After Directory
---require("after.plugin")
-
--- Colors
 --vim.cmd.colorscheme("gruvbox")
