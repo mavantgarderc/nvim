116d50e6c2f804d6d0fc8beea0d1325a7b1568f7
mavantgarderc <manihabibinava@gmail.com>
2025-06-16 05:13:10 +0330

    lualine modified

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,17 +1,74 @@
-return{
+return {
     "nvim-lualine/lualine.nvim",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
+        local hide_in_width = function() return vim.fn.winwidth(0) > 80 end
+
+        local diagnostics = {
+            "diagnostics",
+            sources = { "nvim_diagnostic" },
+            sections = { "error", "warn" },
+            symbols = { error = " ", warn = " " },
+            colored = false,
+            update_in_insert = false,
+            always_visible = true,
+        }
+
+        local diff = {
+            "diff",
+            colored = false,
+            symbols = { added = " ", modified = " ", removed = " " },
+            cond = hide_in_width,
+        }
+
+        -- local mode = {
+        --     "mode",
+        --     fmt = function(str)
+        --         return "-- " .. str .. " --"
+        --     end,
+        -- }
+
+        local filetype = {
+            "filetype",
+            icons_enabled = false,
+            icon = nil,
+        }
+
+        local branch = {
+            "branch",
+            icons_enabled = true,
+            --icon = "",
+            icon = "󰝨",
+        }
+
+        local location = {
+            "location",
+            padding = 0,
+        }
+
+        local progress = function()
+            --local chars = { "_", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" }
+            --local chars = { "▱▱", "▰▱", "▰▰" }
+            local chars = { "⡀ ", "⡄ ", "⡆ ", "⡇ ", "⡇⡀", "⡇⡄", "⡇⡆", "⡇⡇",}
+            local current_line = vim.fn.line(".")
+            local total_lines = vim.fn.line("$")
+            local index = math.ceil((current_line / total_lines) * #chars)
+            return chars[index]
+
+        end
+
+        local spaces = function() return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth") end
+
         require("lualine").setup({
             options = {
                 icons_enabled = true,
                 theme = "auto",
-                component_separators = { left = '', right = ''},
-                section_separators = { left = '', right = ''},
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
                 disabled_filetypes = {
-                    statusline = {},
+                    statusline = { "alpha", "dashboard", "NvimTree", "Outline" },
                     winbar = {},
                 },
                 ignore_focus = {},
@@ -22,27 +79,27 @@ return{
                     statusline = 100,
                     tabline = 100,
                     winbar = 100,
-                }},
+                },
+            },
             sections = {
-                lualine_a = { "mode'"},
-                lualine_b = { "branch" },
-                lualine_c = { "filename" },
-                lualine_x = { "fileformat", "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" }
+                lualine_a = { branch, diagnostics },
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = { diff, filetype },
+                lualine_y = { location },
+                lualine_z = { progress },
             },
             inactive_sections = {
-                lualine_a = {""},
-                lualine_b = {""},
+                lualine_a = {},
+                lualine_b = {},
                 lualine_c = { "filename" },
                 lualine_x = { "location" },
                 lualine_y = {},
-                lualine_z = {}
+                lualine_z = {},
             },
             tabline = {},
             winbar = {},
             inactive_winbar = {},
-            extensions = { "fugitive" }
         })
-    end
+    end,
 }
