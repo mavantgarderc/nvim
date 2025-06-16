2b04067ae06ba0e7642e7b7ba4073ce52497e424
mavantgarderc <manihabibinava@gmail.com>
2025-06-16 08:05:48 +0330

    options fixed

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,38 +4,44 @@ local o = vim.o
 local cmd = vim.cmd
 local keymap = vim.keymap
 local schedule = vim.schedule
-
+local env = vim.env
 
 g.mapleader = " "
 g.maplocalleader = "\\"
 opt.smartindent = true
 keymap.set('n', "<leader>pv", cmd.Ex)
-cmd("let g:loaded_ruby_provider = 0")
-cmd("let g:loaded_perl_provider = 0")
 
+g.loaded_node_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
 
-cmd("set expandtab")
-cmd("set tabstop=4")
-cmd("set softtabstop=4")
-cmd("set shiftwidth=4")
+-- add binaries installed by mason.nvim to path
+--local is_windows = fn.has "win32" ~= 0
+--local sep = is_windows and "\\" or "/"
+--local delim = is_windows and ";" or ":"
+--env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
+
 opt.number = true
 --opt.relativenumber = true
 opt.numberwidth = 2
 
 opt.expandtab = true    -- Convert tabs to spaces
+opt.shiftwidth = 4
+opt.softtabstop =  4
+opt.tabstop = 4
 
 schedule(function()    -- OS-Nvim clipboard sync
     o.clipboard = "unnamedplus"
 end)
 
-
 o.showmode = false -- don't show the mode (shown in lualine)
 
-o.cursorline = true -- show which line cursor is on
+o.cursorline = false -- show which line cursor is on
 
 o.list = true -- display whitespaces
-opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 opt.wrap = false
 opt.swapfile = false
