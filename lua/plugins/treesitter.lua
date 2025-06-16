44db4c4cb2fff6b67661370fdfe684a4cbd7514c
mavantgarderc <manihabibinava@gmail.com>
2025-06-16 06:11:36 +0330

    treesitter modified

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,67 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            vim.g.skip_ts_context_commentstring_module = true
+
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "bash",
+                    "python",
+                    "markdown",
+                    "markdown_inline",
+                    "html",
+                    "css",
+                    "javascript",
+                    "typescript",
+                    "json",
+                    "yaml",
+                    "toml",
+                    "dockerfile",
+                    "sql",
+                    "regex",
+                    "query",
+                },
+                sync_install = false,
+                auto_install = true,
+                ignore_install = { "phpdoc" },
+
+                highlight = {
+                    enable = true,
+                    disable = { "css" },
+                    additional_vim_regex_highlighting = false,
+                },
+
+                indent = {
+                    enable = true,
+                    disable = { "python", "css" },
+                },
+
+                autopairs = { enable = true },
+                incremental_selection = { enable = true },
+                textobjects = { enable = true },
+            })
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+    },
+    {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        event = "VeryLazy",
+        config = function()
+            require("ts_context_commentstring").setup({
+                enable_autocmd = false,
+                languages = {
+                    typescript = "// %s",
+                    javascript = "// %s",
+                    typescriptreact = { __default = "// %s", jsx = "// %s" },
+                },
+            })
+        end,
+    },
 }
