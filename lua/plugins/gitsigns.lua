f615c3cac03f25c3503c40b62b6a11d8268604c5
mavantgarderc <manihabibinava@gmail.com>
2025-06-16 05:12:56 +0330

    gitsigns plugin modified

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,16 +6,36 @@ return {
             local gitsigns = require("gitsigns")
 
             gitsigns.setup({
-                signcolumn = true, -- Show git signs in the sign column
-                numhl     = true,  -- Color the line numbers for changed lines
-                linehl    = false, -- Don't highlight the entire line (set true if you want)
-                word_diff = false,  -- Enable word-level diff highlights
+                signcolumn = true,
+                numhl     = true,
+                linehl    = false,
+                word_diff = false,
                 signs = {
-                    add          = {hl = 'GitSignsAdd',          text = '│', numhl='GitSignsAddNr',    linehl='GitSignsAddLn'},
-                    change       = {hl = 'GitSignsChange',       text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-                    delete       = {hl = 'GitSignsDelete',       text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-                    topdelete    = {hl = 'GitSignsDelete',       text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-                    changedelete = {hl = 'GitSignsChange',       text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+                    add          = {hl = 'GitSignsAdd',    text = '│', numhl='GitSignsAddNr',    linehl='GitSignsAddLn'   },
+                    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+                    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+                    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+                    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+                    --add          = { hl = "GitSignsAdd",    text = " ", numhl = "GitSignsAddNr",    linehl = "GitSignsAddLn"    },
+                    --change       = { hl = "GitSignsChange", text = " ", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+                    --delete       = { hl = "GitSignsDelete", text = " ", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+                    --topdelete    = { hl = "GitSignsDelete", text = "󱅁 ", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+                    --changedelete = { hl = "GitSignsChange", text = "󰍷 ", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+                },
+                watch_gitdir = {
+                    interval = 1000,
+                    follow_files = true,
+                },
+                sign_priority = 6,
+                update_debounce = 100,
+                status_formatter = nil,
+                max_file_length = 40000,
+                preview_config = {
+                    border = "single",
+                    style = "minimal",
+                    relative = "cursor",
+                    row = 0,
+                    col = 1,
                 },
             })
 
