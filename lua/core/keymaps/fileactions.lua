a3fca54f15cc5b0e4f3ea695cd67fa04afa7df07
mavantgarderc <manihabibinava@gmail.com>
2025-06-21 13:19:43 +0330

    +x & -x permission keymap updated

--- lua/core/keymaps/fileactions.lua
+++ lua/core/keymaps/fileactions.lua
@@ -6,8 +6,12 @@ map("x", "<leader>p", '"_dp')
 -- replace the cursor under word, in entire buffer
 map("n", "<leader>s", ":%s/\\<<C-r><C-w>>\\>//gI<Left><Left>")
 
--- add execution permission
+-- execution permission
 map("n", "<leader>x", ":!chmod +x %<CR>", { silent = true })
+map("n", "<leader>X", ":!chmod -x %<CR>", { silent = true })
 
 -- source current file
-map("n", "<leader>o", ":w<CR>:so %<CR>")
+map("n", "<leader>oo", ":wa<CR>:so %<CR>")
+
+-- Quit
+map("n", "<leader>o<leader>O", ":wa<CR>:qa")
