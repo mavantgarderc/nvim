c9b4b1f13a954cceb2c9f11bb6c30b589ed50981
mavantgarderc <manihabibinava@gmail.com>
2025-06-26 09:01:25 +0330

    dap fixed
    still has bug

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,31 +1,87 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "nvim-neotest/nvim-nio",
-    },
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "mfussenegger/nvim-dap-python",
+            "nvim-neotest/nvim-nio",  -- Add this missing dependency
+        },
+        config = function()
+            local dap = require('dap')
+            local dapui = require('dapui')
+            local map = vim.keymap.set
+            local fn = vim.fn
 
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
-        local map = vim.keymap.set
+            dapui.setup()
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
 
-        dapui.setup()
+            map('n', '<F5>', dap.continue)
+            map('n', '<F10>', dap.step_over)
+            map('n', '<F11>', dap.step_into)
+            map('n', '<F12>', dap.step_out)
 
-        map("n", "<leader>dt", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
-        map("n", "<leader>dc", dap.continue, { desc = "DAP: Continue" })
+            map('n', '<Leader>b', dap.toggle_breakpoint)
+            map('n', '<Leader>B', function()
+                dap.set_breakpoint(fn.input('Breakpoint condition: '))
+            end)
+            map('n', '<Leader>lp', function()
+                dap.set_breakpoint(nil, nil, fn.input('Log point message: '))
+            end)
+            map('n', '<Leader>dr', dap.repl.open)
+            map('n', '<Leader>du', dapui.toggle)
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
+            -- Python
+            require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+
+            -- C#
+            dap.adapters.coreclr = {
+                type = 'executable',
+                command = '$HOME/.local/netcoredbg/netcoredbg',
+                args = {'--interpreter=vscode'}
+            }
+            dap.configurations.cs = {
+                {
+                    type = 'coreclr',
+                    name = 'Launch - .NET Core',
+                    request = 'launch',
+                    program = function()
+                        return fn.input('Path to dll: ', fn.getcwd() .. '/bin/Debug/', 'file')
+                    end,
+                },
+            }
+
+            -- JS/TS
+            -- dap.adapters["pwa-node"] = {
+            --     type = "server",
+            --     host = "localhost",
+            --     port = "${port}",
+            --     executable = {
+            --         command = "node",
+            --         args = {os.getenv("HOME") .. "/.vscode-js-debug/src/dapDebugServer.js", "${port}"},
+            --     }
+            -- }
+            -- for _, lang in ipairs({ 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' }) do
+            --     dap.configurations[lang] = {
+            --         {
+            --             type = "pwa-node",
+            --             request = "launch",
+            --             name = "Launch Node",
+            --             program = "${file}",
+            --             cwd = "${workspaceFolder}",
+            --         },
+            --     }
+            -- end
         end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-    end,
+    }
 }
