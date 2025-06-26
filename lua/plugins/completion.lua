0bcd06143d495f24522e23b28060c7efb52d1fd5
mavantgarderc <manihabibinava@gmail.com>
2025-06-26 06:23:07 +0330

    completion icons

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -10,6 +10,7 @@ return {
         config = function()
             local cmp = require("cmp")
             local luasnip = require("luasnip")
+            local g = vim.g
 
             require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -22,6 +23,87 @@ return {
                 window = {
                     completion = cmp.config.window.bordered(),
                     documentation = cmp.config.window.bordered(),
+                },
+                formatting = {
+                    fields = { "kind", "abbr", "menu" },
+                    format = function(entry, vim_item)
+                        local kind_icons = {
+                            Text = "󰉿",
+                            Method = "󰆧",
+                            Function = "󰊕",
+                            Constructor = "",
+                            Field = "󰜢",
+                            Variable = "󰀫",
+                            Class = "󰠱",
+                            Interface = "",
+                            Module = "",
+                            Property = "󰜢",
+                            Unit = "󰑭",
+                            Value = "󰎠",
+                            Enum = "",
+                            Keyword = "󰌋",
+                            Snippet = "",
+                            Color = "󰏘",
+                            File = "󰈙",
+                            Reference = "󰈇",
+                            Folder = "󰉋",
+                            EnumMember = "",
+                            Constant = "󰏿",
+                            Struct = "󰙅",
+                            Event = "",
+                            Operator = "󰆕",
+                            TypeParameter = "",
+                        }
+
+                        if not g.have_nerd_font then
+                            kind_icons = {
+                                Text = "T",
+                                Method = "M",
+                                Function = "F",
+                                Constructor = "C",
+                                Field = "F",
+                                Variable = "V",
+                                Class = "C",
+                                Interface = "I",
+                                Module = "M",
+                                Property = "P",
+                                Unit = "U",
+                                Value = "V",
+                                Enum = "E",
+                                Keyword = "K",
+                                Snippet = "S",
+                                Color = "C",
+                                File = "F",
+                                Reference = "R",
+                                Folder = "D",
+                                EnumMember = "E",
+                                Constant = "C",
+                                Struct = "S",
+                                Event = "E",
+                                Operator = "O",
+                                TypeParameter = "T",
+                            }
+                        end
+
+                        vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind] or "", vim_item.kind)
+
+                        local source_names = {
+                            nvim_lsp = "[LSP]",
+                            luasnip = "[Snippet]",
+                            buffer = "[Buffer]",
+                            path = "[Path]",
+                            nvim_lua = "[Lua]",
+                            cmdline = "[CMD]",
+                        }
+
+                        vim_item.menu = source_names[entry.source.name] or "[" .. entry.source.name .. "]"
+
+                        if #vim_item.abbr > 40 then
+                            vim_item.abbr = string.sub(vim_item.abbr, 1, 37) .. "..."
+                        end
+
+                        return vim_item
+                    end,
                 },
                 mapping = cmp.mapping.preset.insert({
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
