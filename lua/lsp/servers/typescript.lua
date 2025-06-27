68038b3ca8d44eb207e315c2a2676470fcf166c3
mavantgarderc <manihabibinava@gmail.com>
2025-06-28 00:08:49 +0330

    typescript lsp module

---
+++ lua/lsp/servers/typescript.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+function M.setup(capabilities)
+    local lspconfig = require("lspconfig")
+
+    lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        settings = {
+            typescript = {
+                inlayHints = {
+                    includeInlayParameterNameHints = "all",
+                    includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                    includeInlayFunctionParameterTypeHints = true,
+                    includeInlayVariableTypeHints = true,
+                    includeInlayPropertyDeclarationTypeHints = true,
+                    includeInlayFunctionLikeReturnTypeHints = true,
+                    includeInlayEnumMemberValueHints = true,
+                },
+            },
+            javascript = {
+                inlayHints = {
+                    includeInlayParameterNameHints = "all",
+                    includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                    includeInlayFunctionParameterTypeHints = true,
+                    includeInlayVariableTypeHints = true,
+                    includeInlayPropertyDeclarationTypeHints = true,
+                    includeInlayFunctionLikeReturnTypeHints = true,
+                    includeInlayEnumMemberValueHints = true,
+                },
+            },
+        },
+    })
+end
+
+return M
