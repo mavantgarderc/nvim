2d8727ad8bf1ae19ec2dd5de12979bfc8a8ddea1
mavantgarderc <manihabibinava@gmail.com>
2025-06-28 00:08:39 +0330

    latex lsp module

---
+++ lua/lsp/servers/latex.lua
@@ -0,0 +1,36 @@
+local M = {}
+
+function M.setup(capabilities)
+    local lspconfig = require("lspconfig")
+
+    lspconfig.texlab.setup({
+        capabilities = capabilities,
+        settings = {
+            texlab = {
+                auxDirectory = ".",
+                bibtexFormatter = "texlab",
+                build = {
+                    args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+                    executable = "latexmk",
+                    forwardSearchAfter = false,
+                    onSave = false,
+                },
+                chktex = {
+                    onEdit = false,
+                    onOpenAndSave = false,
+                },
+                diagnosticsDelay = 300,
+                formatterLineLength = 80,
+                forwardSearch = {
+                    args = {},
+                },
+                latexFormatter = "latexindent",
+                latexindent = {
+                    modifyLineBreaks = false,
+                },
+            },
+        },
+    })
+end
+
+return M
