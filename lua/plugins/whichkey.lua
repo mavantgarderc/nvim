0929e91a38611c0ddec8db30655e28b8ac3b6ab6
mavantgarderc <manihabibinava@gmail.com>
2025-07-05 16:21:45 +0330

    whichkeyfixed

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,122 +1,74 @@
 return {
     "folke/which-key.nvim",
     event = "VimEnter",
-    opts = {
-        delay = 0,
-
-        plugins = {
-            marks = true,
-            registers = true,
-            spelling = {
-                enabled = true,
-                suggestions = 20,
+    config = function()
+        local status_ok, which_key = pcall(require, "which-key")
+        if not status_ok then return end
+        local g = vim.g
+        local setup = {
+            delay = 0,
+            plugins = {
+                marks = true,
+                registers = true,
+                spelling = {
+                    enabled = true,
+                    suggestions = 20,
+                },
+                presets = {
+                    operators = false,
+                    motions = true,
+                    text_objects = true,
+                    windows = true,
+                    nav = true,
+                    z = true,
+                    g = true,
+                },
             },
-            presets = {
+            show = {
                 operators = false,
-                motions = true,
-                text_objects = true,
-                windows = true,
-                nav = true,
-                z = true,
-                g = true,
             },
-        },
-
-        show = {
-            operators = false,
-        },
-
-        replace = {
-            ["<space>"] = "SPC",
-            ["<cr>"] = "RET",
-            ["<tab>"] = "TAB",
-        },
-
-        icons = {
-            breadcrumb = "»",
-            separator = "➜",
-            group = "+",
-            ellipsis = "…",
-            mappings = vim.g.have_nerd_font ~= false,
-            keys = vim.g.have_nerd_font ~= false and {} or {
-                Up = " ",
-                Down = " ",
-                Left = " ",
-                Right = " ",
-                C = "󰘴 ",
-                M = "󰘵 ",
-                D = "󰘳 ",
-                S = "󰘶 ",
-                CR = "󰌑 ",
-                Esc = "󱊷 ",
-                ScrollWheelDown = "󱕐 ",
-                ScrollWheelUp = "󱕑 ",
-                NL = "󰌑 ",
-                BS = "󰁮",
-                Space = "󱁐 ",
-                Tab = "󰌒 ",
-                F1 = "󱊫",
-                F2 = "󱊬",
-                F3 = "󱊭",
-                F4 = "󱊮",
-                F5 = "󱊯",
-                F6 = "󱊰",
-                F7 = "󱊱",
-                F8 = "󱊲",
-                F9 = "󱊳",
-                F10 = "󱊴",
-                F11 = "󱊵",
-                F12 = "󱊶",
+            key_labels = {
+                ["<space>"] = "SPC",
+                ["<cr>"] = "RET",
+                ["<tab>"] = "TAB",
             },
-        },
-
-        win = {
-            border = "rounded",
-            position = "bottom",
-            margin = { 1, 0, 1, 0 },
-            padding = { 2, 2, 2, 2 },
-            winblend = 0,
-            zindex = 1000,
-        },
-
-        layout = {
-            height = { min = 4, max = 25 },
-            width = { min = 20, max = 50 },
-            spacing = 3,
-            align = "left",
-        },
-
-        keys = {
-            scroll_down = "<c-d>",
-            scroll_up = "<c-u>",
-        },
-
-        sort = { "local", "order", "group", "alphanum", "mod" },
-
-        expand = 0,
-
-        notify = true,
-
-        triggers = {
-            { "<auto>", mode = "nxsot" },
-        },
-
-        disable = {
-            ft = {},
-            bt = {},
-        },
-
-        debug = false,
-    },
-
-    config = function(_, opts)
-        local wk = require("which-key")
-        wk.setup(opts)
-
-        wk.add({
-            { "<leader>s", group = "[S]earch" },
-            { "<leader>t", group = "[T]oggle" },
-            { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
-        })
-    end,
+            icons = {
+                breadcrumb = "»",
+                separator = "➜",
+                group = "+",
+                mappings = g.have_nerd_font,
+                keys = g.have_nerd_font and {} or {
+                },
+            },
+            win = {
+                border = "rounded",
+                position = "bottom",
+                margin = { 1, 0, 1, 0 },
+                padding = { 2, 2, 2, 2 },
+                winblend = 0,
+            },
+            layout = {
+                height = { min = 4, max = 25 },
+                width = { min = 20, max = 50 },
+                spacing = 3,
+                align = "left",
+            },
+            ignore_missing = false,
+            filter = function() return true end,
+            triggers = { "<leader>" },
+            triggers_blacklist = {
+                i = { "j", "k" },
+                v = { "j", "k" },
+            },
+            keys = {
+                scroll_down = "<c-d>",
+                scroll_up = "<c-u>",
+            },
+        }
+        local group_specs = {
+            ["<leader>s"] = { name = "[S]earch", mode = "n" },
+            ["<leader>t"] = { name = "[T]oggle", mode = "n" },
+            ["<leader>h"] = { name = "Git [H]unk", mode = { "n", "v" } },
+        }
+    end
 }
