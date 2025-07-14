337ab45f26adc778cfb3c050a7873f64671b1f12
mavantgarderc <manihabibinava@gmail.com>
2025-07-14 16:38:35 +0330

    feat: core.lua keymap descriptions

--- lua/core/keymaps/core.lua
+++ lua/core/keymaps/core.lua
@@ -1,77 +1,73 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true, }
 
--- Oil
-map("n", "<leader>pv", ":Oil<CR>", opts)
-
 -- Paste from OS-Integrated Clipboard
 -- map("n", "<leader>p", "\"_dp", opts)
 
--- Surround with Character
-map("n", "<leader>g(", "ciw()<C-[>P",    { desc = "Surround word with (); Normal"   })
-map("n", "<leader>g[", "ciw[]<C-[>P",    { desc = "Surround word with []; Normal"   })
-map("n", "<leader>g{", "ciw{}<C-[>P",    { desc = "Surround word with {}; Normal"   })
-map("n", "<leader>g\"", "ciw\"\"<C-[>P", { desc = "Surround word with \"\"; Normal" })
-map("n", "<leader>g'", "ciw''<C-[>P",    { desc = "Surround word with ''; Normal"   })
-map("n", "<leader>g*", "ciw**<C-[>P",    { desc = "Surround word with **; Normal"   })
+-- Oil
+map("n", "<leader>pv", ":Oil<CR>", { noremap = true, silent = true, desc = "Open Oil"})
 
-map("v", "<leader>g(", "c()<C-[>P",      { desc = "Surround word with (); Visual"   })
-map("v", "<leader>g[", "c[]<C-[>P",      { desc = "Surround word with []; Visual"   })
-map("v", "<leader>g{", "c{}<C-[>P",      { desc = "Surround word with {}; Visual"   })
-map("v", "<leader>g\"", "c\"\"<C-[>P",   { desc = "Surround word with \"\"; Visual" })
-map("v", "<leader>g'", "c''<C-[>P",      { desc = "Surround word with ''; Visual"   })
-map("v", "<leader>g*", "c**<C-[>P",      { desc = "Surround word with **; Visual"   })
-
--- Movement
 map({ "n", "v", "i" }, "<Find>",   "0")
 map({ "n", "v", "i" }, "<Select>", "$")
 map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
-map("n", "gg", "ggzt")
-map("n", "<C-b>", "<C-b>zt") -- one   pg up
-map("n", "<C-u>", "<C-u>zz") -- half  pg up
-map("n", "<C-d>", "<C-d>zz") -- half  pg dn
-map("n", "<C-f>", "<C-f>zb") -- one   pg dn
-map("n", "G", "Gzb")
+map("n", "gg", "ggzt", { desc = "Go to Top" })
+map("n", "<C-b>", "<C-b>zt", { desc = "Go one page up"    })
+map("n", "<C-u>", "<C-u>zz", { desc = "Go half page up"   })
+map("n", "<C-d>", "<C-d>zz", { desc = "Go half page down" })
+map("n", "<C-f>", "<C-f>zb", { desc = "Go one page down"  })
+map("n", "G", "Gzb", { desc = "Go to bottom" })
 
-map("i", "<C-b>", "<ESC>^i")
-map("i", "<C-h>", "<Left>")
-map("i", "<C-l>", "<Right>")
-map("i", "<C-j>", "<Down>")
-map("i", "<C-k>", "<Up>")
+map("i", "<C-b>", "<ESC>^i", { desc = "Go bol; 'i' mode"   })
+map("i", "<C-h>", "<Left>",  { desc = "Go left; 'i' mode"  })
+map("i", "<C-l>", "<Right>", { desc = "Go rigth; 'i' mode" })
+map("i", "<C-j>", "<Down>",  { desc = "Go down; 'i' mode"  })
+map("i", "<C-k>", "<Up>",    { desc = "Go up; 'i' mode"    })
 
-map("i", "<C-w>", "<C-w>") -- delete word before cursor
-map("i", "<C-u>", "<C-u>") -- delete line before cursor
+map("i", "<C-w>", "<C-w>", { desc = "Delete word before cursor"})
+map("i", "<C-u>", "<C-u>", { desc = "Delete line before cursor"})
 
-map("i", "<C-t>", "<C-t>") -- indent forward
-map("i", "<C-d>", "<C-d>") -- indent backward
+map("i", "<C-t>", "<C-t>", { desc = "Indent forward"  })
+map("i", "<C-d>", "<C-d>", { desc = "Indent backward" })
 
-map("i", "<C-n>", "<C-n>") -- suggest next
-map("i", "<C-p>", "<C-p>") -- suggest prev
-map("i", "<C-x><C-l>", "<C-x><C-l>") -- open suggestion dropdown
+map("i", "<C-n>", "<C-n>", { desc = "Suggest next" })
+map("i", "<C-p>", "<C-p>", { desc = "Suggest prev" })
+map("i", "<C-x><C-l>", "<C-x><C-l>", { desc = "Open suggestion dropdown"})
 
--- move selected lines vertically
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line(s) down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line(s) up"   })
 
--- Comments
-map("v", "<leader>/", "gc",  { remap = true })
+map("v", "<leader>/", "gc",  { remap = true, desc = "Comment the selected line(s)" })
 
--- stand-still cursor while merging lines
-map("n", "J", "mzJ`z")
+map("n", "J", "mzJ`z", { desc = "Stand still cursor, on merging lines" })
 
 -- searching
 map("n", "/", "/", { desc = "Search forward"  })
 map("n", "?", "?", { desc = "Search backward" })
-map("n", "n", "nzzzv")
-map("n", "N", "Nzzzv")
-map("n", "<Esc>", ":nohlsearch<CR>", opts)
-map("n", "<C-[>", ":nohlsearch<CR>", opts)
+map("n", "n", "nzzzv", { desc = "Next searched item" })
+map("n", "N", "Nzzzv", { desc = "Prev searched item" })
+map("n", "<Esc>", ":nohlsearch<CR>", { noremap = true, silent = true, desc = "Clear the searching highlights" })
+map("n", "<C-[>", ":nohlsearch<CR>", { noremap = true, silent = true, desc = "Clear the searching highlights" })
 
--- duplicate lines
-map("n", "<C-A-S-p>", "mzyyP`zk")
+map("n", "<C-A-S-p>", "mzyyP`zk", { desc = "Duplicate the current line" })
 
 -- Multi-Cursor
-map("n", "<A-S-j>", "<Plug>(VM-Add-Cursor-Down)", opts)
-map("n", "<A-S-k>", "<Plug>(VM-Add-Cursor-Up)",   opts)
+map("n", "<A-S-j>", "<Plug>(VM-Add-Cursor-Down)", { noremap = true, silent = true, desc = "Add cursor down" })
+map("n", "<A-S-k>", "<Plug>(VM-Add-Cursor-Up)",   { noremap = true, silent = true, desc = "Add cursor up"   })
+
+map("n", "<leader>g(",  "ciw()<C-[>P",   { desc = "Surround word with (); Normal"   })
+map("n", "<leader>g[",  "ciw[]<C-[>P",   { desc = "Surround word with []; Normal"   })
+map("n", "<leader>g{",  "ciw{}<C-[>P",   { desc = "Surround word with {}; Normal"   })
+map("n", "<leader>g\"", "ciw\"\"<C-[>P", { desc = "Surround word with \"\"; Normal" })
+map("n", "<leader>g'",  "ciw''<C-[>P",   { desc = "Surround word with ''; Normal"   })
+map("n", "<leader>g*",  "ciw**<C-[>P",   { desc = "Surround word with **; Normal"   })
+map("n", "<leader>g<",  "ciw<><C-[>P",   { desc = "Surround word with <>; Normal"   })
+
+map("v", "<leader>g(",  "c()<C-[>P",     { desc = "Surround word with (); Visual"   })
+map("v", "<leader>g[",  "c[]<C-[>P",     { desc = "Surround word with []; Visual"   })
+map("v", "<leader>g{",  "c{}<C-[>P",     { desc = "Surround word with {}; Visual"   })
+map("v", "<leader>g\"", "c\"\"<C-[>P",   { desc = "Surround word with \"\"; Visual" })
+map("v", "<leader>g'",  "c''<C-[>P",     { desc = "Surround word with ''; Visual"   })
+map("v", "<leader>g*",  "c**<C-[>P",     { desc = "Surround word with **; Visual"   })
+map("n", "<leader>g<",  "c<><C-[>P",     { desc = "Surround word with <>; Normal"   })
