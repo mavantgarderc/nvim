7e9ab243613dfcdcd314c611e7d2b95525c3dfc0
mavantgarderc <manihabibinava@gmail.com>
2025-08-24 01:18:28 +0330

    fix: ci

--- raphael_state.lua
+++ raphael_state.lua
@@ -1,1 +1,1 @@
-return { last_colorscheme = { name = "kanagawa-paper-ink", type = "builtin", timestamp = 1755984683 } }
+return { last_colorscheme = { name = "kanagawa-paper-ink", type = "builtin", timestamp = 1755985659 } }
