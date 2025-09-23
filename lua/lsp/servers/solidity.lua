bbaac290d0f48472b76c13fb2da6f934c15a7efe
mavantgarderc <manihabibinava@gmail.com>
2025-09-23 03:41:32 +0330

    chore(lsp): solidity lsp updated & integrated

--- lua/lsp/servers/solidity.lua
+++ lua/lsp/servers/solidity.lua
@@ -1,35 +1,35 @@
 local shared = require("lsp.shared")
 
 local M = {}
-
-local notify = vim.notify
-local b = vim.b
-local bo = vim.bo
+local api = vim.api
 local lsp = vim.lsp
 local log = vim.log
-local api = vim.api
-local opt_local = vim.opt_local
+local notify = vim.notify
 
 function M.setup()
   local capabilities = shared.get_capabilities()
 
-  vim.lsp.config["solidity_ls"] = {
+  -- define server config once
+  lsp.config["solidity_ls"] = {
     capabilities = capabilities,
+
     on_attach = function(client, bufnr)
-      shared.setup_keymaps()
+      if shared.setup_keymaps then
+        pcall(shared.setup_keymaps, bufnr)
+      end
 
       if client.supports_method("textDocument/formatting") then
         api.nvim_create_autocmd("BufWritePre", {
           buffer = bufnr,
           callback = function()
-            if b.lsp_format_on_save ~= false then
-              lsp.buf.format({
-                bufnr = bufnr,
-                filter = function(c)
-                  return c.name == "solidity_ls"
-                end,
-              })
+            local ok, val = pcall(api.nvim_buf_get_var, bufnr, "lsp_format_on_save")
+            if ok and val == false then
+              return
             end
+            lsp.buf.format({
+              bufnr = bufnr,
+              filter = function(c) return c.name == "solidity_ls" end,
+            })
           end,
         })
       end
@@ -38,30 +38,28 @@ function M.setup()
     settings = {
       solidity = {
         compileUsingRemoteVersion = "latest",
-        compileUsingLocalVersion = "",
         formatter = "forge",
         enabledAsYouTypeCompilationErrorCheck = true,
         validationDelay = 1500,
         packageDefaultDependenciesContractsDirectory = "contracts",
         packageDefaultDependenciesDirectory = "lib",
-        mocha = {
-          enabled = false,
-          optionsPath = "./test/.mocharc.json",
-        },
-        defaultCompiler = "remote", -- "remote", "localFile", "localNodeModule"
-        analysisLevel = "full", -- "full", "basic", "none"
+        defaultCompiler = "remote",
+        analysisLevel = "full",
         enableIncrementalCompilation = true,
       },
     },
 
     filetypes = { "solidity" },
 
-    root_dir = function(fname)
-      return vim.fs.root(
-        fname,
-        { "hardhat.config.js", "hardhat.config.ts", "foundry.toml", "truffle-config.js", "truffle.js", "remappings.txt", ".git" }
-      )
-    end,
+    root_markers = {
+      "hardhat.config.js",
+      "hardhat.config.ts",
+      "foundry.toml",
+      "truffle-config.js",
+      "truffle.js",
+      "remappings.txt",
+      ".git",
+    },
 
     single_file_support = true,
 
@@ -70,9 +68,8 @@ function M.setup()
     },
   }
 
-  -- actually start the server
-  vim.lsp.start(vim.lsp.config["solidity_ls"])
-
+  -- with the new API you don’t call lsp.start manually,
+  -- nvim-lspconfig auto-starts the server when conditions match.
   M.setup_solidity_autocmds()
 end
 
@@ -83,45 +80,33 @@ function M.setup_solidity_autocmds()
     group = augroup,
     pattern = "solidity",
     callback = function()
-      opt_local.tabstop = 4
-      opt_local.shiftwidth = 4
-      opt_local.expandtab = true
-      opt_local.commentstring = "// %s"
-      opt_local.spell = false
-      opt_local.foldmethod = "syntax"
+      vim.opt_local.tabstop = 4
+      vim.opt_local.shiftwidth = 4
+      vim.opt_local.expandtab = true
+      vim.opt_local.commentstring = "// %s"
+      vim.opt_local.spell = false
+      vim.opt_local.foldmethod = "syntax"
     end,
   })
 
   api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     group = augroup,
     pattern = { "*.sol" },
-    callback = function()
-      bo.filetype = "solidity"
+    callback = function(args)
+      vim.bo[args.buf].filetype = "solidity"
     end,
   })
 end
 
-function M.check_solidity_ls()
-  local handle = io.popen("solidity-language-server --version 2>/dev/null")
-  if handle then
-    local result = handle:read("*a")
-    handle:close()
-    if result and result ~= "" then
-      return true
-    end
-  end
-  return false
-end
-
 function M.mason_setup()
-  local mason_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
-  if not mason_ok then
+  local ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+  if not ok then
     notify("mason-lspconfig not found", log.levels.ERROR)
     return
   end
 
   mason_lspconfig.setup({
-    ensure_installed = { "solidity_ls" }, -- must match actual server name
+    ensure_installed = { "solidity_ls" },
     automatic_installation = true,
   })
 end
