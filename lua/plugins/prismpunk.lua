1917a88801a968198bc151d1941bae18ab4f0bd1
mavantgarderc <manihabibinava@gmail.com>
2025-11-09 01:46:15 +0330

    fix(plugins): prismpunk config fixed

--- lua/plugins/prismpunk.lua
+++ lua/plugins/prismpunk.lua
@@ -1,44 +1,47 @@
 return {
   "mavantgarderc/prismpunk.nvim",
-  priority = 1000,
   lazy = false,
-  require("prismpunk").setup({
-    theme = "kanagawa/paper-edo",
+  priority = 1000,
 
-    styles = {
-      comments = { italic = true },
-      keywords = { bold = false },
-      functions = { bold = false },
-      variables = {},
-    },
+  config = function()
+    require("prismpunk").setup({
+      theme = "kanagawa/paper-edo",
 
-    overrides = {
-      colors = {},
-      highlights = {},
-    },
-
-    integrations = {
-      cmp = true,
-      telescope = true,
-      gitsigns = true,
-      lualine = true,
-    },
-
-    terminal = {
-      enabled = true,
-      emulator = { "ghostty", "alacritty" },
-
-      ghostty = {
-        enabled = true,
-        auto_reload = true,
-        config_path = vim.fn.expand("~/.config/ghostty/themes/prismpunk.toml"),
+      styles = {
+        comments = { italic = true },
+        keywords = { bold = false },
+        functions = { bold = false },
+        variables = {},
       },
 
-      alacritty = {
-        enabled = true,
-        auto_reload = true,
-        config_path = vim.fn.expand("~/.config/alacritty/prismpunk.toml"),
+      overrides = {
+        colors = {},
+        highlights = {},
       },
-    },
-  }),
+
+      integrations = {
+        cmp = true,
+        telescope = true,
+        gitsigns = true,
+        lualine = true,
+      },
+
+      terminal = {
+        enabled = true,
+        emulator = { "ghostty", "alacritty" },
+
+        ghostty = {
+          enabled = true,
+          auto_reload = true,
+          config_path = vim.fn.expand("~/.config/ghostty/themes/prismpunk.toml"),
+        },
+
+        alacritty = {
+          enabled = true,
+          auto_reload = true,
+          config_path = vim.fn.expand("~/.config/alacritty/prismpunk.toml"),
+        },
+      },
+    })
+  end,
 }
