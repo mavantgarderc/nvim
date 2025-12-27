33d469fd6c2dbf19e33672e2503b645cdc0cce82
mavantgarderc <manihabibinava@gmail.com>
2025-12-27 08:12:35 +0330

    chore(lint): stylua applied

--- lua/plugins/cmpconfig.lua
+++ lua/plugins/cmpconfig.lua
@@ -116,8 +116,8 @@ return {
 			cmp.setup.cmdline({ "/", "?" }, {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = {
-					{ name = "buffer" }
-				}
+					{ name = "buffer" },
+				},
 			})
 
 			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore)
@@ -125,8 +125,8 @@ return {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
 					{ name = "path" },
-					{ name = "cmdline" }
-				})
+					{ name = "cmdline" },
+				}),
 			})
 		end,
 	},
