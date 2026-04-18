385a5eec6a8a1226ce4c02d5d7fa0f8c879594d5
mavantgarderc <manihabibinava@gmail.com>
2026-04-18 06:39:52 +0330

    fix(lsp): replace runtime_file scan w/ scan with explicit workspace library in lua_ls

--- lua/lsp/servers/lua_ls.lua
+++ lua/lsp/servers/lua_ls.lua
@@ -22,7 +22,10 @@ function M.setup(capabilities)
 					globals = { "vim" },
 				},
 				workspace = {
-					library = vim.api.nvim_get_runtime_file("", true),
+					library = {
+						vim.env.VIMRUNTIME,
+						"${3rd}/luv/library",
+					},
 					checkThirdParty = false,
 				},
 				telemetry = {
